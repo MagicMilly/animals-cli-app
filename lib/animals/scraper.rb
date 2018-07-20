ee33690a8a8d1b62207a65ed1907b47c15897a32
Emily J Cain <emilycain@ymail.com>
2018-07-20 00:05:57 +0000

    initial scraping code

--- lib/animals/scraper.rb
+++ lib/animals/scraper.rb
@@ -2,5 +2,13 @@
 
 class Animals::Scraper
 
+  MAIN_URL = "http://leilanifarmsanctuary.com/meet-the-ducks/"
+
+    def self.scrape_animal_profiles
+      ducks = []
+      page = Nokogiri::HTML(open(MAIN_URL))
+      duck_name = page.css("h4 strong")
+      binding.pry
+    end
 
 end
