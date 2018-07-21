3f8f4e3df74681d8bd4796744e198c9dcd232207
Emily J Cain <emilycain@ymail.com>
2018-07-21 21:07:29 +0000

    duck_name scraping ability

--- lib/animals/scraper.rb
+++ lib/animals/scraper.rb
@@ -7,7 +7,8 @@ class Animals::Scraper
     def self.scrape_animal_profiles
       ducks = []
       page = Nokogiri::HTML(open(BASE_URL))
-      duck_name = page.css("h4 strong").text
+      duck_names = page.css("h4 strong").map {|name| name.text}
+      # duck_descriptions = page.css("h4 p").map {|description| description.text}
       binding.pry
       puts "end"
     end
