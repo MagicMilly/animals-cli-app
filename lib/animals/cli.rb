1882743d4a75aed141b564c8cbf03574f59b4bb7
Emily J Cain <emilycain@ymail.com>
2018-07-21 21:45:55 +0000

    initial CLI functioning with duck names

--- lib/animals/cli.rb
+++ lib/animals/cli.rb
@@ -2,6 +2,18 @@
 
 class Animals::CLI
 
+  def start
+    puts "Welcome!"
+    Animals::Scraper.scrape_duck_names 
+    main_menu
+  end
 
+  def main_menu
+    puts "Here are the ducks living at Leilani Farm Sanctuary:"
+    ducks = Animals::Ducks.all
+    ducks.each.with_index(1) do |duck, index|
+      puts "#{index}. #{duck.name}"
+    end
+  end
 
 end
--- lib/animals/scraper.rb
+++ lib/animals/scraper.rb
@@ -4,13 +4,17 @@ class Animals::Scraper
 
   BASE_URL = "http://leilanifarmsanctuary.com/meet-the-ducks/"
 
-    def self.scrape_animal_profiles
+    def self.scrape_duck_names
       ducks = []
       page = Nokogiri::HTML(open(BASE_URL))
-      duck_names = page.css("h4 strong").map {|name| name.text}
-      # duck_descriptions = page.css("h4 p").map {|description| description.text}
-      binding.pry
-      puts "end"
+      duck_names = page.css("h4 strong").map {|duck_name| duck_name.text}
+      duck_names.each do |name|
+        Animals::Ducks.new(name)
+      end
     end
+
+    # def self.scrape_duck_bios
+    #   duck_descriptions = page.css("h4 p").map {|description| description.text}
+    # end
 
 end
