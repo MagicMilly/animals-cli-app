1ecfda71b8cf6067c46854919c8ee1ae1ccee1e1
Emily J Cain <emilycain@ymail.com>
2018-07-21 21:56:40 +0000

    Added prompt to select a duck

--- lib/animals/cli.rb
+++ lib/animals/cli.rb
@@ -4,7 +4,8 @@ class Animals::CLI
 
   def start
     puts "Welcome!"
-    Animals::Scraper.scrape_duck_names 
+    Animals::Scraper.scrape_duck_names
+    puts " "
     main_menu
   end
 
@@ -14,6 +15,12 @@ class Animals::CLI
     ducks.each.with_index(1) do |duck, index|
       puts "#{index}. #{duck.name}"
     end
+    puts " "
+    puts "Please select a duck's number to learn more about that duck!"
+
+    # User input code 
+    # input = gets.strip
+    # index = input_to_i - 1
   end
 
 end
